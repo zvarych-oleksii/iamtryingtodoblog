3f493c9311f66210757fb041186dd1489bffc2fa
zvarych-oleksii <oleksii.zvarych@lnu.edu.ua>
2023-02-08 21:22:04 +0200

    Changed DataBase to Postgresql

--- iamtryingtodoblog/settings.py
+++ iamtryingtodoblog/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'default',
-   'users'
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -79,14 +79,16 @@ WSGI_APPLICATION = 'iamtryingtodoblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES ={
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "newdatabase",
+        "USER" : "BlogUser",
+        "PASSWORD" : "1Dalpinesony",
+        "HOST" : "localhost",
+        "PORT" : "5432",
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
